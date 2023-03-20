c94fb328ed268dddf27918844d63d58d6fd5388b
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2023-03-20 01:43:06 -0500

    stacks and Queue

---
+++ week3/palindrome.py
@@ -0,0 +1,22 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, e):
+        self.items.append(e)
+
+    def pop(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.items[-1]
