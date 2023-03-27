ae3f4545b8918cd294d23cc4241635c24cebfca9
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2023-03-27 00:09:46 -0500

    Hashing

--- week4/hashing.py
+++ week4/hashing.py
@@ -6,12 +6,20 @@ class HashTable:
 
     def hashfunction(self,key):
         # Insert your hashing function code
+        return key % self.size
 
     def rehash(self,key):
         # Insert your secondary hashing function code
+        return key // self.size
 
     def put(self,key,data):
         # Insert your code here to store key and data 
+        hashValue = self.hashfunction(key)
+        rehashValue = self.rehash(key)
+
+        if self.slots[hashValue] is None:
+            self.slots[hashValue] = key
+            self.data[hashValue] = data
 
     def get(self,key):
         # Insert your code here to get data by key
@@ -24,12 +32,22 @@ class HashTable:
 
 
 H = HashTable()
-H[69] = 'A'
+
 # Store remaining input data
-
+H[69] = 'A'
+H[66] = 'B'
+H[80] = 'C'
+H[35] = 'D'
+H[18] = 'E'
+H[52] = 'F'
+H[89] = 'G'
+H[70] = 'H'
+H[12] = 'I'
 # print the slot values
-
+print(H.slots)
 # print the data values
+print(H.data)
 
 # print the value for key 52
+print(H[52])
 
