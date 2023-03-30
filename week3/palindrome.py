91e5b4edc176b854687af4302a65776d5aa2a044
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2023-03-29 23:55:50 -0500

    UPDATED Stacks & Queue

--- week3/palindrome.py
+++ week3/palindrome.py
@@ -21,4 +21,46 @@ class Stack:
             return None
         return self.items[-1]
 
-class Queue
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if self.isEmpty():
+            return None
+        return self.items.pop(0)
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.items == 0
+
+    def peek(self):
+        if self.isEmpty():
+            return None
+        return self.items[0]
+
+
+def isPalindrome(string):
+    my_stack = Stack()
+    queue = Queue()
+
+    for character in string:
+        my_stack.push(character)
+        queue.enqueue(character)
+
+    while not my_stack.isEmpty():
+        if my_stack.pop() != queue.dequeue():
+            return False
+
+    return True
+
+print(isPalindrome("racecar"))  # expected output: True
+print(isPalindrome("noon"))    # expected output: True
+print(isPalindrome("python"))  # expected output: False
+print(isPalindrome("madam"))   # expected output: True
